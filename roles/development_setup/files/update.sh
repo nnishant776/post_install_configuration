ca0192bc49ef7387ffcab852e86e54077795d6ee
Neeraj Nishant <nn54565852@gmail.com>
2022-10-29 04:13:51 +0530

    Add script for downloading and extracting VSCode

---
+++ roles/development_setup/files/update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e -u -x
+
+# download latest visual studio code package
+curl --proto "=https" -X GET -L "https://code.visualstudio.com/sha/download?build=stable&os=linux-x64" -o /tmp/vscode-linux-x64.tar.gz
+
+# extract the downloded application tar
+mkdir -p ~/.local/apps
+tar -C ~/.local/apps -xvf /tmp/vscode-linux-x64.tar.gz
+rm /tmp/vscode-linux-x64.tar.gz
+
+# install symlink in discoverable PATH
+ln -sf ~/.local/apps/VSCode-linux-x64/bin/code ~/.local/bin/code
